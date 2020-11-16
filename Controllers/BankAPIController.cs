c14b03c305c276b8c7c5bbfa254f6f5f2f3a2b82
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-11-16 10:14:22 -0700

    Added BankAPI Controller;

---
+++ Controllers/BankAPIController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Capstone_VV.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BankAPIController : ControllerBase
+    {
+    }
+}
