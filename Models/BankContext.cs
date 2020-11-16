1141623016cf61afef10ecf7c9e5646cb4f66d1a
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-11-15 19:55:51 -0700

    Setting up BankContext.cs;

---
+++ Models/BankContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone_VV.Models
+{
+    public class BankContext : DbContext
+    {
+        public virtual DbSet<Client> Clients { get; set; }
+        public virtual DbSet<Account> Accounts { get; set; }
+        public virtual DbSet<Transaction> Transactions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                string connection =
+                    "server=localhost;" +
+                    "port=3306;" +
+                    "user=root;" +
+                    "database=bank_db;";
+
+                string version = "10.4.14-MariaDB";
+
+                optionsBuilder.UseMySql(connection, x => x.ServerVersion(version));
+            }
+        }
+
+        // Model Builder stuff goes here, along with seed data
+
+    }
+}
