d8f2653807a4fb3c9f1cca9558f9a38855758692
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-12-01 02:05:00 -0700

    Added random string generation (with reference on how to change type) for transactionID;

--- ClientApp/src/components/CreateTransaction.jsx
+++ ClientApp/src/components/CreateTransaction.jsx
@@ -4,14 +4,15 @@ import "./css/CreateNotification.css"
 
 
 function CreateTransaction(props) {
-    
+    const randomString = require('random-string');    // https://www.npmjs.com/package/random-string
+    const x = randomString(); // x contains now a random String with the length of 8
 
     return (
         <section className="notification-page">
             <section className="notification-message">
                 <h1 className="notification-header" className="display-3"> Transaction Created! </h1>
                 <p id="notification-paragraph" className="lead"><strong>Thank You for banking with us!</strong></p>
-                <p id="notification-paragraph" className="lead">Your Transaction has been successfully added to the database.</p>
+                <p id="notification-paragraph" className="lead">Your Transaction has been successfully added to the database. Your unique TransactionID is: <strong>{x}</strong>.</p>
                 <button className="btn btn-info">
                     <Link className="white-text" to="/landing-page">
                         Return To Landing Page
