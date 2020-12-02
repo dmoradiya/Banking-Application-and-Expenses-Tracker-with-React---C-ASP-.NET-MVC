25a4498c59d686cf7ae5cfed56e59278dc5075ce
dhmec <dharmoradiya@gmail.com>
2020-12-01 22:25:47 -0700

    Added css in Create deposit page

--- ClientApp/src/components/CreateDeposit.jsx
+++ ClientApp/src/components/CreateDeposit.jsx
@@ -85,7 +85,7 @@ function CreateDeposit(props) {
                                         <option value=""  >Select Account</option>
                                             {accountInfo.map(client => (<option key={client.accountID} value={`${client.accountID}`}>
                                             {`${client.accountType}- Total Balance: $${(client.accountBalance + client.cashback).toFixed(2)}`}</option>
-                ))}
+                                             ))}
                                     </select>
                             </section>
                             <section className="input-group-prepend deposit-prepend">
@@ -101,7 +101,7 @@ function CreateDeposit(props) {
                                     <input className="form-control" id="transactionValue" placeholder="Transaction Value" type="text" onChange={handleFieldChange} />
                             </section>
                             <section className="deposit-submit">
-                                <input type="submit" className="btn btn-primary dropdown-toggle" value="Submit" />
+                                <input type="submit" className="btn btn-info dropdown-toggle" value="Submit" />
                             </section>
                         </form>
         </section>
