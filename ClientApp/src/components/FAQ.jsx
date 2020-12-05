e5bbf00d2244f3ef8907396013a0ad85a37d0f85
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-12-05 10:00:47 -0700

    Updated FAQ.css;

--- ClientApp/src/components/FAQ.jsx
+++ ClientApp/src/components/FAQ.jsx
@@ -14,7 +14,7 @@ function FAQ(props) {
                     <p>A: This application allows you to enter transaction data, and displays a visual representation of your expenses.</p>
                     <p className="form-control" id="faq-paragraph lead"><strong> Q: How do I know my funds are safe here?</strong></p>
                     <p>A: We use state of the art technology to ensure your data is safe and secure at all times. Just trust us :)</p>
-                    <p className="form-control" id="faq-paragraph lead"><strong> Q: What does a good expense ratio look like?</strong></p>
+                    <p className="form-control" id="faq-paragraph lead"><strong> Q: What do good expenses look like?</strong></p>
                     <p>A: According to creditcanada.com, 35% Housing, 15% Food, 15% Transportation, 10% Utilities, 10% Debt Payments, 5% Savings, 2.5% Clothing, 2.5% Medical, 5% Other is a good benchmark!</p>
                     <p className="form-control" id="faq-paragraph lead"><strong> Q: What can I do to save more money?</strong></p>
                     <p>A: Create a budget. Our planners would be happy to assist you. See our contact page for more details!</p>
